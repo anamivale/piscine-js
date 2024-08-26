3d09a0f2a3fcaceb6eea919b78d400bc92227aba
vmuhembe <muhembelev@gmail.com>
2024-08-26 15:12:29 +0300

    fix: fix how to access object properties

--- change.js
+++ change.js
@@ -1,8 +1,18 @@
+const sourceObject = {
+    num: 42,
+    bool: true,
+    str: 'some text',
+    log: console.log,
+  }
+
+  
 function get(a) {
-  return sourceObject["a"];
+  return sourceObject[a];
 }
 
 function set(a, val) {
-  sourceObject.a = val;
-  return sourceObject.a;
+  sourceObject[a] = val;
+  return sourceObject[a];
 }
+
+
