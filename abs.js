8527e6670df71d6054e83259fc852837ef6b16fd
vmuhembe <muhembelev@gmail.com>
2024-08-27 09:05:57 +0300

    feat: add abs.js; logical opperator

---
+++ abs.js
@@ -0,0 +1,13 @@
+function isPositive(param) {
+  if (param >= 0) {
+    return true;
+  }
+  return false;
+}
+
+function abs(params) {
+  if (params >= 0) {
+    return params;
+  }
+  return params * -1;
+}
