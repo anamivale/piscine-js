86626db266829256306061ea27d83306c256ed67
vmuhembe <muhembelev@gmail.com>
2024-08-27 09:18:19 +0300

    feat: add min-max.js thatimplements min and max functions

---
+++ min-max.js
@@ -0,0 +1,13 @@
+function max(param1, param2) {
+  if (param1 > param2) {
+    return param1;
+  }
+  return param2;
+}
+
+function min(param1, param2) {
+  if (param1 < param2) {
+    return param1;
+  }
+  return param2;
+}
