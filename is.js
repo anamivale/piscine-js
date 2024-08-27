f43f7944ccb83b073972d41fc943edd98e780449
vmuhembe <muhembelev@gmail.com>
2024-08-27 10:18:08 +0300

    feat: add is.js check datatypes of variables

---
+++ is.js
@@ -0,0 +1,47 @@
+const is = {};
+
+is.num = (val) => {
+  return typeof val === "number";
+};
+is.nan = (val) => {
+  return isNaN(val);
+};
+is.str = (val) => {
+  return typeof val === "string";
+};
+is.bool = (val) => {
+  return typeof val === "boolean";
+};
+
+is.undef = (val) => {
+  return typeof val === "undefined";
+};
+
+is.def = (val) => {
+  return !(typeof val === "undefined");
+};
+is.arr = (val) => {
+  return Array.isArray(val);
+};
+
+is.obj = (val) => {
+  return typeof val === "object";
+};
+is.fun = (val) => {
+  return typeof val === "function";
+};
+is.truthy = (val) => {
+  if (val) {
+    return true;
+  }
+  return false;
+};
+
+is.falsy = (val) => {
+  if (val) {
+    return false;
+  }
+  return true;
+};
+
+console.log(is.falsy(1));
