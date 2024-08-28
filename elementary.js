204875b72db2920accf37646cbe8897f008b283c
vmuhembe <muhembelev@gmail.com>
2024-08-28 20:25:45 +0300

    fix: handle the negative numbers

--- elementary.js
+++ elementary.js
@@ -1,28 +1,75 @@
 const multiply = (a, b) => {
-   let  output = 0
-    for (let i = 1; i<=b; i++){
-        output += a
+  if (a < 0 && b < 0) {
+    a = -a;
+    b = -b;
+    let output = 0;
+    for (let i = 1; i <= b; i++) {
+      output += a;
     }
-    return output
-};
+    return output;
+  }
+  if (a < 0) {
+    a = -a;
+    let output = 0;
+    for (let i = 1; i <= b; i++) {
+      output += a;
+    }
+    return -output;
+  }
+  if (b < 0) {
+    b = -b;
+    let output = 0;
+    for (let i = 1; i <= b; i++) {
+      output += a;
+    }
+    return -output;
+  }
 
+  let output = 0;
+  for (let i = 1; i <= b; i++) {
+    output += a;
+  }
+  return output;
+};
 
 const divide = (a, b) => {
-    let x = 0
-    while (a>0){
-        a -= b
-        x++
-    }
-    return x
-    
+  if (a < 0) {
+    a = -a;
+    let x = 0;
+  while (a > 0) {
+    a -= b;
+    x++;
+  }
+  return -x;
+  }
+  if (b < 0) {
+    b = -b;
+    let x = 0;
+  while (a > 0) {
+    a -= b;
+    x++;
+  }
+  return -x;
+  }
+  let x = 0;
+  while (a > 0) {
+    a -= b;
+    x++;
+  }
+  return x;
 };
 const modulo = (a, b) => {
-    let x = 0
-    while (a>b-1){
-        a -= b
-        x = a
-    }
-    return x
-    
+  if (a < 0) {
+    a = -a;
+  }
+  if (b < 0) {
+    b = -b;
+  }
+  let x = 0;
+  while (a > b - 1) {
+    a -= b;
+    x = a;
+  }
+  return x;
 };
-
+console.log(divide(-12, 6));
