5b7f5b4bd20bfc7539689813ebeea00f80340068
vmuhembe <muhembelev@gmail.com>
2024-08-28 22:06:07 +0300

    feat: add slice.js to implement arr.slice

---
+++ slicer.js
@@ -0,0 +1,18 @@
+const slice = (a, b, c) => {
+  if (typeof c !== "undefined") {
+    const output = [];
+    for (let i = b; i < c; i++) {
+      output.push(a[i]);
+    }
+    return output;
+  }
+
+  const output = [];
+  for (let i = b; i < a.length; i++) {
+    output.push(a[i]);
+  }
+
+  return output;
+};
+a = [1, 2, 3, 4, 5];
+
