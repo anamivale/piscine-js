42f45ed38cdf2d5d72ecc4134b758b4da3cd115f
vmuhembe <muhembelev@gmail.com>
2024-08-28 22:15:29 +0300

    differentiate the slicer for array and that of string

--- slicer.js
+++ slicer.js
@@ -1,17 +1,33 @@
 const slice = (a, b, c) => {
-  if (typeof c !== "undefined") {
+  if (Array.isArray(a)) {
+    if (typeof c !== "undefined") {
+      const output = [];
+      for (let i = b; i < c; i++) {
+        output.push(a[i]);
+      }
+      return output;
+    }
+
     const output = [];
-    for (let i = b; i < c; i++) {
+    for (let i = b; i < a.length; i++) {
       output.push(a[i]);
     }
     return output;
   }
-
-  const output = [];
-  for (let i = b; i < a.length; i++) {
-    output.push(a[i]);
+  if (typeof c !== "undefined") {
+    let output = "";
+    for (let i = b; i < c; i++) {
+      output += a[i];
+    }
+    return output;
   }
 
+  let output = "";
+  for (let i = b; i < a.length; i++) {
+    output += a[i];
+  }
   return output;
 };
+a = [1, 2, 3, 4, 5];
 
+console.log(slice("abcdef", 2));
