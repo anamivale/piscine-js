178167eb320bbf700df29c0fc4a74b318a3d8ced
vmuhembe <muhembelev@gmail.com>
2024-08-28 20:09:58 +0300

    implement */% without using them

--- elementary.js
+++ elementary.js
@@ -1,11 +1,28 @@
 const multiply = (a, b) => {
-  return a * b;
+   let  output = 0
+    for (let i = 1; i<=b; i++){
+        output += a
+    }
+    return output
 };
+
 
 const divide = (a, b) => {
-  return a / b;
+    let x = 0
+    while (a>0){
+        a -= b
+        x++
+    }
+    return x
+    
+};
+const modulo = (a, b) => {
+    let x = 0
+    while (a>b-1){
+        a -= b
+        x = a
+    }
+    return x
+    
 };
 
-const modulo = (a, b) => {
-  return a % b;
-};
