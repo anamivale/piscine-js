f49820feabdf734acc3824711582eccd1c2e7817
vmuhembe <muhembelev@gmail.com>
2024-08-29 17:23:52 +0300

    solve infinite loop

--- reverser.js
+++ reverser.js
@@ -2,17 +2,17 @@ function reverse(params) {
     
     if (Array.isArray(params)){
         let output = []
-        for (let i = params.length; i>0; i--){
+        for (let i = params.length-1; i>=0; i--){
             output.push(params[i])
         }
         return output
     }
 
     let output = ""
-        for (let i = params.length; i>0; i--){
+        for (let i = params.length-1; i>=0; i--){
             output += params[i]
         }
+        return output
     
 }
 
-console.log(reverse("valeria"));
