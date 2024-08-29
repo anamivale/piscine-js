05a3a01e7bc9996af2c62ea6d76d8b056c84e37e
vmuhembe <muhembelev@gmail.com>
2024-08-29 12:40:25 +0300

    feat: add cut-corners.js that work similar wit math.round, math.ceil, math.floor, math.trunc

---
+++ cut-corners.js
@@ -0,0 +1,65 @@
+function trunc(params) {
+  return parseInt(params);
+}
+
+function round(params) {
+  let b = 0;
+  let sign = false;
+  if (params < 0) {
+    sign = true;
+    params = -params;
+  }
+  while (b + 1 < params) {
+    b++;
+  }
+  if (sign) {
+    x = params - b;
+    if (x >= 0.5) {
+      b++;
+    }
+    return -b;
+  }
+  x = params - b;
+  if (x >= 0.5) {
+    b++;
+  }
+  return b;
+}
+
+function floor(params) {
+  let b = 0;
+  let sign = false;
+  if (params < 0) {
+    sign = true;
+    params = -params;
+  }
+  while (b + 1 < params) {
+    b++;
+  }
+  if (sign) {
+    return -b - 1;
+  }
+
+  return b;
+}
+
+function ceil(params) {
+  let b = 0;
+  let sign = false;
+  if (params < 0) {
+    sign = true;
+    params = -params;
+  }
+  while (b < params) {
+    b++;
+  }
+  if (sign) {
+    return -b + 1;
+  }
+
+  return b;
+}
+const nums = [3.7, -3.7, 3.1, -3.1];
+console.log(nums.map(round));
+console.log(nums.map(floor));
+console.log(nums.map(trunc));
