48ba36f7ce71216cb78454586275fc971ab9706c
vmuhembe <muhembelev@gmail.com>
2024-08-29 13:20:04 +0300

    fix

--- cut-corners.js
+++ cut-corners.js
@@ -53,8 +53,4 @@ function trunc(params) {
   }
   return sign ? -b : b;}
 
-const nums = [3.7, -3.7, 3.1, -3.1];
-console.log(nums.map(round));
-console.log(nums.map(floor));
-console.log(nums.map(trunc));
-console.log(nums.map(ceil));
+
