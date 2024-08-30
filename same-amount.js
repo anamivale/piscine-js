f1b1c04290d8de86ece52715d17b6444b98ced16
vmuhembe <muhembelev@gmail.com>
2024-08-31 02:18:22 +0300

    solve the question

--- same-amount.js
+++ same-amount.js
@@ -1,7 +1,7 @@
 function sameAmount(params, args1, args2) {
   let x = params.match(new RegExp(args1, "g"));
   let y = params.match(new RegExp(args2, "g"));
-  console.log(x);
+  console.log("Param: ",params,"ARGS1: ", args1,"ARGS2: ", args2);
   if (x === null || y === null) {
     return false;
   }
@@ -16,19 +16,3 @@ data2 =
   "qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd";
 
 console.log(sameAmount(data2, /q /, /qqqqqqq/));
-
-// const regexp = /test1/g;
-// const str = "test1test1";
-
-// str.match(regexp); // ['test1', 'test2']
-
-// console.log(str.match(regexp));
-// // console.log(array[1]);
-
-// let re = /ab+c/; // literal notation
-// // OR
-// // re = new RegExp("ab+c", "i"); // constructor with string pattern as first argument
-// console.log(re)
-// // OR
-// re = new RegExp(re, "g");
-// console.log(re)
