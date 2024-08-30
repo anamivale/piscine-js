befac0ae07947e9d19189548e31cced3000c4690
vmuhembe <muhembelev@gmail.com>
2024-08-30 15:01:28 +0300

    solve the question

--- nasa.js
+++ nasa.js
@@ -1,10 +1,10 @@
 function nasa(params) {
    
-  let output = "";
+  let output = "\"";
 
   for (let i = 1; i <= params; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
-      if (i == params.length - 1) {
+      if (i === params) {
         output += "NASA";
         continue;
       }
@@ -12,7 +12,7 @@ function nasa(params) {
       continue;
     }
     if (i % 3 == 0) {
-      if (i == params.length - 1) {
+      if (i == params) {
         output += "NA";
         continue;
       }
@@ -20,7 +20,7 @@ function nasa(params) {
       continue;
     }
     if (i % 5 == 0) {
-      if (i == params.length - 1) {
+      if (i == params) {
         output += "SA";
         continue;
       }
@@ -28,12 +28,12 @@ function nasa(params) {
       continue;
     }
     output += i;
-    if (i !== params.length - 1) {
+    if (i !== params) {
         output += " ";
       }
 
   }
-  return output
+  return output + "\""
 }
 
 console.log(nasa(15));
