2d102948e99b311c32bcc8439ba7fb5a39e99d11
vmuhembe <muhembelev@gmail.com>
2024-08-30 20:55:38 +0300

    solve the question

---
+++ its-a-match.js
@@ -0,0 +1,8 @@
+let normal = /hi/;
+// Find a match at the beginning/end of a word, beginning
+// like this: \bHI, end like this: HI\b
+// \B 	Find a match, but not at the beginning/end of a word
+
+let begin = /^hi/;
+let end = /hi$/;
+let beginEnd = /^hi$/;
