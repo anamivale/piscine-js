46184fdb5a6ed63a7a72b6f3dd61a189a57ae9b3
vmuhembe <muhembelev@gmail.com>
2024-08-30 23:13:00 +0300

    solve the question

--- vowel-dots.js
+++ vowel-dots.js
@@ -2,7 +2,7 @@ function vowelDots(params) {
   let output = "";
 
   for (let i = 0; i < params.length; i++) {
-    const vowels = /[aeiouAEIOU]/;
+    let vowels = /[aeiouAEIOU]/;
 
     if (vowels.test(params[i])) {
       output += params[i] + ".";
