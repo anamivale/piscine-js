4f6ecb0a535777e6c181b4871416f030751080cd
vmuhembe <muhembelev@gmail.com>
2024-08-31 00:47:00 +0300

    solve the question

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,9 @@
 function sameAmount(params, args1, args2) {
   let x = params.match(args1);
   let y = params.match(args2);
+  if (x === null || y===nul){
+    return false
+  }
   if (x.length === y.length) {
     return true;
   }
