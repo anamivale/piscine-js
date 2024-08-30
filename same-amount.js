951723b98e6bd815bd10552ee0a92df87d1350ba
vmuhembe <muhembelev@gmail.com>
2024-08-31 01:15:00 +0300

    solve the question

--- same-amount.js
+++ same-amount.js
@@ -1,10 +1,10 @@
 function sameAmount(params, args1, args2) {
-    console.log(params);
+  console.log(params);
   let x = params.match(args1);
   let y = params.match(args2);
-  console.log(x,y);
-  if (x === null || y===null){
-    return false
+  console.log(x.length, y.length);
+  if (x === null || y === null) {
+    return false;
   }
   if (x.length === y.length) {
     return true;
@@ -12,3 +12,7 @@ function sameAmount(params, args1, args2) {
   return false;
 }
 
+data2 =
+  "qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd";
+
+console.log(sameAmount(data2, /q /, /qqqqqqq/));
