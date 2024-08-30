6529936f48c94bb4d8969f5fb12b473aba27fc11
vmuhembe <muhembelev@gmail.com>
2024-08-30 19:16:50 +0300

    solve the question

---
+++ pyramid.js
@@ -0,0 +1,17 @@
+function pyramid(str, depth) {
+    let output = ""
+    let strLen = str.length
+    for (let i=1; i<=depth; i++){
+        let st = str.repeat((i*2)-1)
+        let base = strLen*(2*depth -1)
+        let level = strLen*(2*i -1)
+
+        let spaces = " ".repeat((base-level)/2)
+        output += spaces + st
+        if (i!= depth){
+            output += "\n"
+        }
+    }
+
+    return output
+}
