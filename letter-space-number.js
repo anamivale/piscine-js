3eb61331b596d0a72c178c639cd4faf747d6fe4a
vmuhembe <muhembelev@gmail.com>
2024-08-30 22:02:40 +0300

    solve the question

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,7 +1,7 @@
 function letterSpaceNumber(params) {
   let output = [];
   let regx = /[a-zA-Z] [0-9](?![a-zA-Z0-9])/g;
-  x = params.match(regx);
+  let x = params.match(regx);
   if (x) {
     output = x;
   }
