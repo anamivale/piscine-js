9945dbcb60bf47fadf479ba1c9c5e4a998b88d3f
vmuhembe <muhembelev@gmail.com>
2024-08-31 16:05:44 +0300

    solve the question

--- greedy-url.js
+++ greedy-url.js
@@ -1,9 +1,10 @@
 // getURL: returns all URLs present in the dataSet
 function getURL(params) {
-    let regex = /(https?:\/\/)[a-z0-9.\/]+/g
+    let regex = /(https:\/\/|http:\/\/)([\w.\/\S]+)/g
     let x = params.match(regex)
- 
-    return x
+    let y = []
+    y = x
+    return y
 }
 // greedyQuery: returns URLs from the dataSet, with at least 3 query parameters.
 function greedyQuery(params) {
@@ -15,4 +16,4 @@ function greedyQuery(params) {
 }
 // notSoGreedy: returns URLs from the dataSet, with at least 2, but not more then 3 query parameters.
 function notSoGreedy(params) {}
-console.log(getURL("qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you"));
+console.log(getURL("qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you http://www.example.com/catalog.asp?itemid=232&template=fresh&crcat=ppc&crsource=google&crkw=buy-a-lot http://example.com/path?name=Branch&products=[Journeys,Email,Universal%20Ads]interact" ));
