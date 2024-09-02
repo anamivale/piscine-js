d6c356c65ac71c5cf8b0053df012b64cfb8fcd75
vmuhembe <muhembelev@gmail.com>
2024-09-02 15:15:13 +0300

    solve the question

--- date-is.js
+++ date-is.js
@@ -1,23 +1,24 @@
 function isValid(date) {
-  if (date instanceof Date) {
-    console.log("g");
-    if (date.toDateString() !== "Invalid Date") {
-      return true;
-    } else{
-      return false
-    }
-  }
-  let d1 = /[a-zA-Z0-9:+()]/
-  if (d1.test(date)){
-    return true
-  }
-  if (date instanceof Date) {
-    console.log("g");
-    if (date.toDateString() !== "Invalid Date") {
-      return true;
-    }
-  }
-  return false;
+  // if (date instanceof Date) {
+  //   console.log("g");
+  //   if (date.toDateString() !== "Invalid Date") {
+  //     return true;
+  //   } else{
+  //     return false
+  //   }
+  // }
+  // let d1 = /[a-zA-Z0-9:+()]/
+  // if (d1.test(date)){
+  //   return true
+  // }
+  // if (date instanceof Date) {
+  //   console.log("g");
+  //   if (date.toDateString() !== "Invalid Date") {
+  //     return true;
+  //   }
+  // }
+  // return false;
+  return date instanceof Date && !isNaN(date.getTime());
 }
 function isAfter(date1, date2) {
   return date1 > date2;
@@ -42,7 +43,7 @@ function isPast(date1) {
   }
   return false;
 }
-console.log(typeof Date(), Date("2006-10-12"));
+// console.log(typeof Date(), ("2006-10-12"));
 
-console.log(isValid(new Date("")));
-console.log(typeof Date(), new Date(""));
+// console.log(isValid(new Date()));
+// console.log(typeof Date(), new Date(""));
