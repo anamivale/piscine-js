8e379bad54667cdcb5d7b14b54c896ea49603aff
vmuhembe <muhembelev@gmail.com>
2024-09-03 10:24:51 +0300

    solve the question

--- get-some-time.js
+++ get-some-time.js
@@ -1,38 +1,42 @@
 function firstDayWeek(week, year) {
-console.log(week, year);
-let date = new Date(year);
+  console.log(week, year);
+  let date = new Date(year);
 
-date.setDate(date.getDate() +  ( week * 1) );
-let y = date.getDay();
-// console.log(y);
-let z = date.getDate()
+  date.setDate(date.getDate() + week * 1);
+  let y = date.getDay();
+  // console.log(y);
+  let z = date.getDate();
 
-switch (y) {
-  case 0:
-    z = z;
-    break;
-  case 1:
-    z = z-1;
-    break;
-  case 2:
-    z = z- 2;
-    break;
-  case 3:
-    z = z- 3;
-    break;
-  case 4:
-    z = z- 4;
-    break;
-  case 5:
-    z = z- 5;
-    break;
-  case 6:
-    z = z- 6;
-    break;
+  switch (y) {
+    case 0:
+      z = z;
+      break;
+    case 1:
+      z = z - 1;
+      break;
+    case 2:
+      z = z - 2;
+      break;
+    case 3:
+      z = z - 3;
+      break;
+    case 4:
+      z = z - 4;
+      break;
+    case 5:
+      z = z - 5;
+      break;
+    case 6:
+      z = z - 6;
+      break;
+  }
+
+  return (
+    String(date.getDate()) +
+    "-" +
+    String(date.getMonth() + 1) +
+    "-" +
+    String(date.getFullYear())
+  );
 }
-
-
-
-return String(date.getFullYear()) + "-" + String(date.getMonth()+1) + "-" + String(date.getDate());
-}
-console.log(firstDayWeek(1, "2000"))
+console.log(firstDayWeek(1, "2000"));
