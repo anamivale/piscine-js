46b5931590e99ad5f8addf341b294c89aa0359de
vmuhembe <muhembelev@gmail.com>
2024-09-03 12:51:01 +0300

    solve the question

--- rebecca-black.js
+++ rebecca-black.js
@@ -1,19 +1,27 @@
 function isFriday(params) {
-
-    return params.getDay() === 5
-    
+  return params.getDay() === 5;
 }
 
 function isWeekend(params) {
-    return params.getDay() === 0 || params.getDay() === 6
+  return params.getDay() === 0 || params.getDay() === 6;
 }
 
 function isLeapYear(params) {
-   return  params.getFullYear() % 4 === 0
-    
+  return params.getFullYear() % 4 === 0;
 }
 function isLastDayOfMonth(params) {
+  if (params.getMonth() === 1) {
+    if (isLeapYear(params)) {
+      if (params.getDate() === 29) {
+        return true;
+      }
+      return false;
+    }
+    if (params.getDate() === 28) {
+      return true;
+    }
+    return false;
+  }
 
-    return params.getDate() === 30 || params.getDate() === 31 
-    
+  return params.getDate() === 30 || params.getDate() === 31;
 }
