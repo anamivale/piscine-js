d5c225d09046984e755e75ead5cafe050d31c4db
vmuhembe <muhembelev@gmail.com>
2024-09-04 15:46:05 +0300

    solve the question

--- for-each.js
+++ for-each.js
@@ -1,7 +1,7 @@
 function forEach(arr, fn) {
     let output = [];
   for (let index = 0; index < arr.length; index++) {
-    fn(arr[index], i, arr);
+    fn(arr[index], index, arr);
   }
   return output;
 }
