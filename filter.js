7462fdd24c247f39968ace714dc154ac1b4be238
vmuhembe <muhembelev@gmail.com>
2024-09-04 19:42:47 +0300

    solve the question

---
+++ filter.js
@@ -0,0 +1,25 @@
+function filter(arr, fn) {
+  let output = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(fn(arr[i], i, arr));
+  }
+  return output;
+}
+
+function reject(arr, fn) {
+  let output = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(!fn(arr[i], i, arr));
+  }
+  return output;
+}
+
+function reject(arr, fn) {
+  let output = [];
+  let neg = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(fn(arr[i], i, arr));
+    neg.push(!fn(arr[i], i, arr));
+  }
+  return [output, neg];
+}
