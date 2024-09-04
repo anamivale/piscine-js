1fb6324b864c9dda2f0bad939e10d464409b4eec
vmuhembe <muhembelev@gmail.com>
2024-09-04 14:51:47 +0300

    solve the question

--- long-words.js
+++ long-words.js
@@ -1,16 +1,21 @@
 function longWords(params) {
+  console.log("pa=>", params);
   let output = params.filter((el) => el.length >= 5);
 
   return params.length === output.length;
 }
 
 function oneLongWord(params) {
+  console.log("ra=>", params);
+
   let output = params.filter((el) => el.length >= 10);
 
   return output.length >= 1;
 }
 
 function noLongWords(params) {
+  console.log("ms=>", params);
+
   let output = params.filter((el) => el.length >= 7);
 
   return output.length < 1;
