a341056174f53d0006d1e2c3e448be9fec0474cb
vmuhembe <muhembelev@gmail.com>
2024-09-04 16:02:50 +0300

    solve the question

--- mapper.js
+++ mapper.js
@@ -1,11 +1,19 @@
 function map(arr, fn) {
+  let output = [];
   for (let index = 0; index < arr.length; index++) {
-    fn(arr[index], index, arr);
+    output.push(fn(arr[index]));
   }
+  return output;
 }
 
 function flatMap(arr, fn) {
+  let output = [];
   for (let index = 0; index < arr.length; index++) {
-    fn(arr[index], index, arr);
+    if (Array.isArray(arr[i])) {
+      output.push(...arr[i]);
+    } else {
+      output.push(arr[i]);
+    }
   }
+  return output;
 }
