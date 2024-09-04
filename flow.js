99c3af80997c532a3be0120170f4a2de8e8a6d4f
vmuhembe <muhembelev@gmail.com>
2024-09-04 23:24:54 +0300

    solve the question

--- flow.js
+++ flow.js
@@ -1,9 +1,8 @@
 function flow(fns) {
-  let initial = fns[0];
-
-  for (let i = 1; i < fns.length; i++) {
-    let x = fns[i](initial);
-    initial = x;
-  }
-  return initial;
+  return function (...args) {
+    if (args.length > 1) {
+      args = [fns[0](...args)];
+    }
+    return fns.reduce((acc, fn) => fn(acc), args[0]);
+  };
 }
