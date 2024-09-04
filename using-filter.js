8025b328066eeea25c69005829d786e451ad9408
vmuhembe <muhembelev@gmail.com>
2024-09-04 13:42:58 +0300

    solve the question

--- using-filter.js
+++ using-filter.js
@@ -31,10 +31,10 @@ function filter1DistinctVowel(params) {
     let count = {};
     for (let i = 0; i < el.length; i++) {
       if (vowels.includes(el[i].toLowerCase())) {
-        count[el[i].toLowerCase()] = el[i]
+        count[el[i].toLowerCase()] = el[i];
       }
     }
-    let arr =Object.keys(count)
+    let arr = Object.keys(count);
     if (arr.length === 1) {
       output.push(el);
     }
@@ -42,5 +42,42 @@ function filter1DistinctVowel(params) {
   return output;
 }
 
+function multiFilter(params) {
+  let vowels = /[aeiouAEIOU]/;
+  let vowel = "aeiouAEIOU";
 
 
+  let output = params.filter(
+    (el) =>
+      el.capital.length >= 8 &&
+      vowel.includes(el.name[0]) &&
+      vowels.test(el.tag) &&
+      el.region !== "South"
+  );
+  return output
+}
+
+console.log(
+  multiFilter([
+    { tag: "CA", name: "California", capital: "Sacramento", region: "West" },
+    { tag: "HI", name: "Hawaii", capital: "Honolulu", region: "West" },
+    {
+      tag: "MO",
+      name: "oMissouri",
+      capital: "Jefferson City",
+      region: "Midwest",
+    },
+    {
+      tag: "PA",
+      name: "Pennsylvania",
+      capital: "Harrisburg",
+      region: "Northeast",
+    },
+    {
+      tag: "RI",
+      name: "Rhode Island",
+      capital: "Providence",
+      region: "Northeast",
+    },
+  ])
+);
