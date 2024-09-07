686b6bc694e64d7456ee33fd1112c9da2c3832bf
vmuhembe <muhembelev@gmail.com>
2024-09-07 11:23:42 +0300

    solve the question

---
+++ fifty-shades-of-cold.js
@@ -0,0 +1,41 @@
+import { colors } from "./fifty-shades-of-cold.data.js";
+
+export function generateClasses() {
+  const styl = document.createElement("style");
+  colors.forEach((element) => {
+    let x = document.createTextNode(`.${element} {
+        background: ${element};
+      }`);
+    styl.appendChild(x);
+  });
+
+  const head = document.querySelector("head");
+  head.append(styl);
+}
+
+export function generateColdShades() {
+  const body = document.querySelector("body");
+  colors.forEach((element) => {
+    if (
+      element.includes("aqua") ||
+      element.includes("blue") ||
+      element.includes("turquoise") ||
+      element.includes("green") ||
+      element.includes("cyan") ||
+      element.includes("navy") ||
+      element.includes("purple")
+    ) {
+      const div = document.createElement("div");
+      div.classList.add(element);
+      body.append(div);
+    }
+  });
+}
+
+export function choseShade(params) {
+  const divs = document.querySelectorAll("div");
+
+  divs.forEach((element) => {
+    element.className = params;
+  });
+}
