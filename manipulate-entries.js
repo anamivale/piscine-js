4d472ac765ac3c29c7075dca0335fc6882019c0f
vmuhembe <muhembelev@gmail.com>
2024-09-11 09:42:15 +0300

    fix filter

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -1,34 +1,34 @@
 function filterEntries(obj, calbackfn) {
-    let result = {};
-  
-   for (const [k,v] of Object.entries(obj)) {
-    if(obj.hasOwnProperty(k)){
-      let [newK , newV ] = calbackfn([k,v])
-        result[newK] = newV;
+  let result = {};
+
+  for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+      if (calbackfn([key, obj[key]])) result[key] = obj[key];
     }
-   
-   }
-    return result;
   }
-  
-  function mapEntries(obj, calbackfn) {
-    const newObj = {}
-    for (let [k,v] of Object.entries(obj)){
-      if(obj.hasOwnProperty(k)){
-        let [newK , newV ] = calbackfn([k,v])
-          newObj[newK] = newV;
-      }
+  console.log(result);
+  return result;
+}
+
+function mapEntries(obj, calbackfn) {
+  const newObj = {};
+  for (let [k, v] of Object.entries(obj)) {
+    if (obj.hasOwnProperty(k)) {
+      let [newK, newV] = calbackfn([k, v]);
+      newObj[newK] = newV;
     }
-    return newObj
   }
-  
-  function reduceEntries(obj, calbackfn, initialValue) {
-    let entries = Object.entries(obj);
-    let acc = typeof initialValue !== "undefined" ? initialValue : entries[0];
-    let start = typeof initialValue !== "undefined" ? 0 : 1;
-    for (let i = start; i < entries.length; i++) {
-      acc = calbackfn(acc, entries[i]);
-    }
-    return acc;
+  return newObj;
+}
+
+function reduceEntries(obj, calbackfn, initialValue) {
+  let entries = Object.entries(obj);
+  let acc = typeof initialValue !== "undefined" ? initialValue : entries[0];
+  let start = typeof initialValue !== "undefined" ? 0 : 1;
+  for (let i = start; i < entries.length; i++) {
+    acc = calbackfn(acc, entries[i]);
   }
-  
+  return acc;
+}
+// const groceriesCart = { orange: 500, oil: 20, sugar: 480 };
+// filterEntries(groceriesCart, ([, v]) => v < 300);
