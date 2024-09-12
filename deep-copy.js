5433864b6514251da76c920792b1d6cda1a44499
vmuhembe <muhembelev@gmail.com>
2024-09-12 09:19:30 +0300

    deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -2,14 +2,14 @@ function deepCopy(params) {
   if (params.constructor === Array) {
     let output = [];
     for (let i = 0; i < params.length; i++) {
-      output[i] = params[i];
+      output[i] = deepCopy(input[i]);
     }
     return output;
   }
   if (params.constructor === Object) {
     let output = {};
     for (const key in params) {
-        output[key] = params[key]
+      output[key] = deepCopy(input[key]);
     }
     return output;
   }
