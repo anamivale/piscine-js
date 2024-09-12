26ef4582359e4cf936d965001f611ad599857c72
vmuhembe <muhembelev@gmail.com>
2024-09-12 09:52:50 +0300

    replica

--- replica.js
+++ replica.js
@@ -1,12 +1,26 @@
-function replica(...params) {
-  if (params.length === 1) {
-    return params;
-  }
-  let output = {};
-  for (let i = 0; i < params.length; i++) {
-    for (const key in params[i]) {
-      output[key] = params[i][key];
-    }
-  }
+var is = {};
+is.arr = (n) => Array.isArray(n);
+is.obj = (n) =>
+  typeof n === "object" &&
+  !is.fun(n) &&
+  !is.arr(n) &&
+  n !== null &&
+  !(n instanceof RegExp);
+is.fun = (n) => typeof n === "function";
+function replica(output, ...inputs) {
+  inputs.forEach((source) => {
+    Object.keys(source).forEach((key) => {
+      if (is.obj(source[key])) {
+        if (!output.hasOwnProperty(key) || !is.obj(output[key])) {
+          output[key] = {};
+        }
+        replica(output[key], source[key]);
+      } else {
+        output[key] = source[key];
+      }
+    });
+  });
   return output;
 }
+
+console.log(replica({ a: { b: 1, c: 2 } }, { a: { c: 23 } }));
