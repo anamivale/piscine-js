3bbeec06ec3aa091df3427f38e7f0d4426240a18
vmuhembe <muhembelev@gmail.com>
2024-09-13 14:13:54 +0300

    lodash.debaunce

---
+++ debounce.js
@@ -0,0 +1,16 @@
+// const _ = require("lodash");
+// const debounce = (str) =>{
+
+// }
+// const processInput = _.debounce(function (text) {
+//   console.log("User input:", text);
+// }, 3000, { leading: false, trailing: true});
+// processInput("name")
+// // Example: Attach debounce to an input field
+// inputElement.addEventListener("input", (e) => {
+//   processInput(e.target.value);
+// });
+
+const debounce = setTimeout(() => {}, 3000);
+
+const opDebounce = setTimeout(() => {}, 0);
