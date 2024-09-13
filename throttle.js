54de2785d35a03ed8fc4a2163724d387d25adf43
vmuhembe <muhembelev@gmail.com>
2024-09-13 16:58:14 +0300

    throttle

--- throttle.js
+++ throttle.js
@@ -1,14 +1,34 @@
-function throttle(func, wait) {
-  let timeout = true;
-
-  return function (...args) {
-    if (timeout) {
-      return;
-    }
-    func(...args);
-
-    setTimeout(() => {
-      timeout = false;
-    }, wait);
-  };
+function throttle(fn, delay) {
+	let last = 0;
+	return function (...args) {
+		const now = Date.now();
+		if (now - last >= delay) {
+			fn.apply(this, args);
+			last = now;
+		}
+	};
+}
+function opThrottle(fn, delay, { leading = false, trailing = true } = {}) {
+	let last = 0;
+	let timer = null;
+	return function () {
+		const now = +new Date();
+		if (!last && leading === false) {
+			last = now;
+		}
+		if (now - last > delay) {
+			if (timer) {
+				clearTimeout(timer);
+				timer = null;
+			}
+			fn.apply(this, arguments);
+			last = now;
+		} else if (!timer && trailing !== false) {
+			timer = setTimeout(() => {
+				fn.apply(this, arguments);
+				last = +new Date();
+				timer = null;
+			}, delay);
+		}
+	};
 }
