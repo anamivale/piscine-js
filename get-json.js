3144bb72b2799322e3cf38fe543d5335b506b824
vmuhembe <muhembelev@gmail.com>
2024-09-14 11:38:41 +0300

    fix

--- get-json.js
+++ get-json.js
@@ -1,26 +1,25 @@
-async function getJSON(path, params) {
-  let url = path;
-
-  try {
-    url = new URL(path);
-  } catch (error) {
-    return error;
-  }
-  console.log(param);
-  let param = URLSearchParams(url.search);
-
-  param.append(params);
-
-  try {
-    const res = await fetch(param);
-
-    if (!res.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    const json = res.json();
-    return json;
-  } catch (error) {
-    console.error(error.message);
-  }
+async function getJSON(path = "", params = {}) {
+	const url =
+		path +
+		"?" +
+		Object.keys(params)
+			.map((key) => {
+				return (
+					key.replace(" ", "+") + "=" + params[key].toString().replace(" ", "+")
+				);
+			})
+			.join("&");
+	const res = await fetch(url).then((response) => {
+		if (response.ok) {
+			return response.json();
+		} else {
+			throw new Error(response.statusText);
+		}
+	});
+	if (res.error) {
+		throw new Error(res.error);
+	}
+	return res.data;
 }
+
 console.log(getJSON("/test", { query: "hello world", b: 5 }));
