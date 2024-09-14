df161c6f78d13e586af6048aa696ba0211ebd870
vmuhembe <muhembelev@gmail.com>
2024-09-14 15:53:35 +0300

    promise

--- series.js
+++ series.js
@@ -1,6 +1,8 @@
 function series(args) {
-  let output = args.map( (element) =>  element());
+  let output = args.map(async (element) => await element());
   return Promise.all(output);
 }
 
-console.log(series([async () => 1, async () => true]));
+series([async () => 1, async () => true]).then((result) => {
+  console.log(result); 
+});
