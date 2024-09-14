c472344f74e529ea8a3a860ca2f3234885d85e3a
vmuhembe <muhembelev@gmail.com>
2024-09-14 10:44:04 +0300

    promises

---
+++ keep-trying-or-giveup.js
@@ -0,0 +1,33 @@
+function retry(count, callback) {
+  return async function (...args) {
+    let attempts = 0;
+    while (true) {
+      try {
+        return await callback(...args);
+      } catch (error) {
+        attempts++;
+        if (attempts > count) {
+          throw error;
+        }
+      }
+    }
+  };
+}
+function timeout(delay, callback) {
+  return async function (...args) {
+    return new Promise((resolve, reject) => {
+      const timer = setTimeout(() => {
+        reject(new Error("timeout"));
+      }, delay);
+      callback(...args)
+        .then((result) => {
+          clearTimeout(timer);
+          resolve(result);
+        })
+        .catch((error) => {
+          clearTimeout(timer);
+          reject(error);
+        });
+    });
+  };
+}
