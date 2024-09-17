59760aba2306cdf77b388795458ad17c0a4c4072
vmuhembe <muhembelev@gmail.com>
2024-09-17 21:41:05 +0300

    read write read

---
+++ tell-me-vip.mjs
@@ -0,0 +1,55 @@
+import fs from "fs";
+
+let path = "/home/vmuhembe/piscine-js/guests";
+
+try {
+  fs.readdir(path, (err, files) => {
+    if (err) {
+      console.log("error>", err.message);
+    }
+
+    files.forEach((element) => {
+      fs.readFile(path + "/" + element, (err, data) => {
+        if (err) {
+          console.log("error>", err.message);
+        }
+        let obj = JSON.parse(data);
+        if (obj.answer === "yes") {
+          fs.appendFile("vip.txt", element, (err) => {
+            if (err) {
+              console.log(err);
+            }
+          });
+        }
+      });
+    });
+  });
+} catch (error) {
+  console.log(error.message);
+}
+try {
+  fs.readFile("vip.txt", (err, data) => {
+    if (err) {
+      console.log("error>", err.message);
+    }
+
+    let dat = data.toString();
+    let arrData = dat.split(".json");
+    let lastFirst = [];
+    for (let file of arrData) {
+      let x = file.split("_");
+      let lf = x[1] + " " + x[0];
+      lastFirst.push(lf);
+    }
+
+    lastFirst.sort();
+    for (let i = 0; i < lastFirst.length - 1; i++) {
+      if (lastFirst[i] === "undefined") {
+        continue;
+      }
+      console.log(`${i + 1}. ${lastFirst[i]}`);
+    }
+  });
+} catch (error) {
+  console.log(error.message);
+}
