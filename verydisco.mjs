4388dcc72c2b908bec93f3134a2269a0cde7c1a0
vmuhembe <muhembelev@gmail.com>
2024-09-17 19:08:12 +0300

    args

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,12 +1,12 @@
 import { argv } from "node:process";
 // print process.argv
 let newArray = [];
-argv.slice(2).forEach((val) => {
+let x = argv[2].split(" ");
+x.forEach((val) => {
   val.length % 2 === 0
     ? newArray.push(val.slice(val.length / 2) + val.slice(0, val.length / 2))
     : newArray.push(
         val.slice(val.length / 2 + 1) + val.slice(0, val.length / 2 + 1)
       );
-
 });
 console.log(newArray.join(" "));
