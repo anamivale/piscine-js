d516306edf0c7b1c4d146797d804a3b798f35f63
vmuhembe <muhembelev@gmail.com>
2024-09-17 18:56:02 +0300

    all args

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,13 +1,12 @@
 import { argv } from "node:process";
 // print process.argv
 let newArray = [];
-argv.slice(2).forEach((val, index) => {
+argv.forEach((val) => {
   val.length % 2 === 0
     ? newArray.push(val.slice(val.length / 2) + val.slice(0, val.length / 2))
     : newArray.push(
         val.slice(val.length / 2 + 1) + val.slice(0, val.length / 2 + 1)
       );
 
-  console.log(`${index}: ${val}`);
 });
 console.log(newArray.join(" "));
