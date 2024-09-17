cc5e8c6272f5f690e404feca989ca3f2cbef7b78
vmuhembe <muhembelev@gmail.com>
2024-09-17 15:28:16 +0300

    use esc6 format

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,8 +1,12 @@
-const { argv } = require("process");
+import { argv } from "node:process";
 // print process.argv
 let newArray = [];
 argv.slice(2).forEach((val, index) => {
-  val.length%2 === 0 ? newArray.push(val.slice((val.length / 2)) + val.slice(0, (val.length / 2))):newArray.push(val.slice((val.length / 2)+1) + val.slice(0, (val.length / 2)+1));
+  val.length % 2 === 0
+    ? newArray.push(val.slice(val.length / 2) + val.slice(0, val.length / 2))
+    : newArray.push(
+        val.slice(val.length / 2 + 1) + val.slice(0, val.length / 2 + 1)
+      );
 
   console.log(`${index}: ${val}`);
 });
